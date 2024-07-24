9bb7b9b6d3906105014362d2db0b81392d3fede0
nitesh2403 <117987347+nitesh2403@users.noreply.github.com>
2024-07-24 19:56:16 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
-  'https://funny-capybara-4982c8.netlify.app/'
+  'https://funny-capybara-4982c8.netlify.app'
 
 ];
 
